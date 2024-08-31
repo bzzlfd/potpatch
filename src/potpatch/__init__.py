2c4bdc05ee9d301c62a639736f6c89297053a831
Zikai Zhou <zzkai@csrc.ac.cn>
2024-09-01 01:59:40 +0800

    [a] add `potpatch.shift` into `__init__`

--- src/potpatch/__init__.py
+++ src/potpatch/__init__.py
@@ -2,11 +2,14 @@ from potpatch.version import __version__
 
 from potpatch import utils
 from potpatch.constant import BOHR, HA, EPSILON0
-from potpatch.objects import (Lattice, 
+from potpatch.objects import (MaterialSystemInfo, 
                               VR, AtomConfig, VATOM, EIGEN, 
-                              MaterialSystemInfo)
+                              Lattice)
 from potpatch.correction import gen_charge_correct, edge_match_correct
-from potpatch.patch import (patch, patch_vr, patch_atom, patch_atom_v2,
+from potpatch.patch import (patch, 
+                            patch_atom, patch_atom_v2, patch_vr, 
                             inspect_ingredient)
 
 from potpatch.supercell import modify_supercell, closed_to_edge
+from potpatch.shift import (shift_materialsystem, 
+                            shift_atomconfig, shift_vr)
