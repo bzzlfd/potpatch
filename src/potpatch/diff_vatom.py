4b30509d19408e046275cfdc47ca9df4d296c49f
Zikai Zhou <zzkai@csrc.ac.cn>
2024-11-25 04:43:03 +0800

    [f] fix bug: diff_vatom bulk using supcl.n123
    abstact `get_window`
    refine `gaussian_integrate`

--- src/potpatch/diff_vatom.py
+++ src/potpatch/diff_vatom.py
@@ -6,6 +6,8 @@ the bulk.vatom
 
 using Gaussian weighted averages, which is a different implementation 
 from the PWmat `OUT.VATOM`
+
+in guassian_integrate and get_window, we use the same ε scaling
 """
 from os.path import exists
 from itertools import product
@@ -17,7 +19,8 @@ from numba import jit, guvectorize, prange
 
 from potpatch.objects import Lattice, VR, AtomConfig, MaterialSystemInfo
 from potpatch.atompos_coin import bulk_order_mapto_supcl
-from potpatch.datatype import INTEGER
+from potpatch.supercell import infer_supercell_size
+from potpatch.datatype import INTEGER, INTEGER_8
 from potpatch.constant import HA, BOHR
 from potpatch.utils import timing
 
@@ -25,6 +28,7 @@ from potpatch.utils import timing
 @timing()
 def diff_vatom(bulk: MaterialSystemInfo, supcl: MaterialSystemInfo, 
                sigma: float = 1.):
+    "calulate and return in atomic unit: r, ξ, diff_vatom "
     ac_bulk  = bulk.atomconfig
     ac_supcl = supcl.atomconfig
     vr_bulk  = bulk.vr
@@ -32,48 +36,60 @@ def diff_vatom(bulk: MaterialSystemInfo, supcl: MaterialSystemInfo,
     epsilon = supcl.epsilon
     n123 = supcl.vr.n123
 
+    # mag
+    _, mag_f = infer_supercell_size(bulk.lattice, supcl.lattice)
+
     # gaussian integration window
-    AL = supcl.lattice.in_unit("atomic unit")
-    iAL = (np.linalg.inv(AL))
-    iuAL = (iAL / np.linalg.norm(iAL, axis=0)).T
-    L = np.array([np.abs(np.dot(AL[i], iuAL[i])) for i in range(3)])
-    window = INTEGER(6 * sigma / L * n123) + 1
-    if any(window > n123):
-        print(f"Warning: window size({window=}) exceeds supercell size{n123=}")
-    window[window > n123] = n123[window > n123]  # TODO edge case
-    w1, w2, w3 = window // 2
-    window_lb = - np.array([w1, w2, w3])
-    window_ur = np.array([w1, w2, w3])
+    window = get_window(supcl.lattice.in_unit("atomic unit"), 
+                        n123, epsilon, sigma)
+    if debug := False:
+        window = np.ones(3, dtype=np.int64) * 2
 
     order, _, _ = bulk_order_mapto_supcl(ac_bulk, ac_supcl, warn_tol=np.inf)
     r          = np.zeros(ac_supcl.natoms, dtype=np.float64)
     ξ          = np.zeros(ac_supcl.natoms, dtype=np.float64)
     diff_vatom = np.zeros((ac_supcl.natoms, 3), dtype=np.float64)
-    print(f"order={order}")
     for i in prange(ac_supcl.natoms):
         # bulk
         pos = ac_bulk.positions[order[i]]
-        center = INTEGER(np.ceil(pos * n123))
         _, _, diff_vatom[i, 0] = \
             gaussian_integrate(bulk.lattice.in_unit("atomic unit"),
-                               vr_bulk.mesh, 
-                               center+window_lb, center+window_ur, 
-                               pos, epsilon, sigma)
+                               vr_bulk.mesh, window, pos, epsilon, sigma)
         # supercell, r, ξ
         pos = ac_supcl.positions[i]
-        center = INTEGER(np.ceil(pos * n123))
         r[i], ξ[i], diff_vatom[i, 1] = \
             gaussian_integrate(supcl.lattice.in_unit("atomic unit"),
-                               vr_supcl.mesh, 
-                               center+window_lb, center+window_ur, 
-                               pos, epsilon, sigma)
+                               vr_supcl.mesh, window, pos, epsilon, sigma)
 
     diff_vatom[:, 2] = diff_vatom[:, 1] - diff_vatom[:, 0]
 
     return r, ξ, diff_vatom, ac_bulk, ac_supcl, order
 
 
-def gaussian_integrate(AL, mesh: np.ndarray, window_lb, window_ur, 
+def get_window(AL, n123, epsilon, sigma):
+    """
+    epsilon supports cos_theta, scaling is not needed here
+    """
+    iAL = (np.linalg.inv(AL))
+    iuAL = (iAL / np.linalg.norm(iAL, axis=0)).T
+
+    L, cos_theta = np.zeros(3), np.zeros(3)
+    for i in range(3):
+        a, p = AL[i], iuAL[i]
+        cos_theta[i] = p.T @ epsilon @ p / norm(p) / norm(epsilon @ p)
+        L[i] = np.dot(p, a)
+    if debug := False:
+        print(f"{L=}, {cos_theta=}")
+    L = L / cos_theta
+
+    window = INTEGER(6 * sigma / L * n123) + 1
+
+    return window
+
+
+# from potpatch.utils import gen_counter
+# cnt = gen_counter()
+def gaussian_integrate(AL, mesh: np.ndarray, window,
                        pos, epsilon, sigma):
     """
     pos in [-0.5, 0.5), corresponding to the image charge correction
@@ -81,30 +97,40 @@ def gaussian_integrate(AL, mesh: np.ndarray, window_lb, window_ur,
     n(ξ) = 1/√(ε₁ε₂ε₃) √(p/π)³ exp(-p ξ²) 
         ξ    = √(r₁²/ε₁ + r₂²/ε₂ + r₃²/ε₃)
         p    = εₘₐₓ/(2σ²)
+    
+    ε is scaled by the `min(ε)`
     """
     Ω = abs(det(AL))
     n123 = np.array(mesh.shape)
     dΩ = Ω / prod(n123)
     
+    # epsilon
     inv_eps = inv(epsilon)
     eps_ii = eigvalsh(epsilon)
+    # |
+    meps_ii = min(eps_ii)
+    # |
+    inv_eps = inv_eps * meps_ii
+    eps_ii = eps_ii / meps_ii
     
+    # indeces
+    center = INTEGER_8(np.ceil(pos * n123))
+    window_lb, window_ur = center - window // 2, center + window // 2
     mesh_indices = np.mgrid[window_lb[0]:window_ur[0], 
                             window_lb[1]:window_ur[1], 
                             window_lb[2]:window_ur[2]]  #
-
     flat_indices = mesh_indices.reshape(3, -1).T  # [m, 3]
-    flat_indices = flat_indices % n123 
 
-    r = pos @ AL.T
+    r = pos @ AL
     ξ, r = sqrt(r.T @ inv_eps @ r), norm(r)
     
-    rT = (flat_indices / n123 - pos) @ AL.T  # [m, 3]
+    rT = (flat_indices / n123 - pos) @ AL  # [m, 3]
     ξ2 = einsum("ij,ij->i", rT @ inv_eps, rT)
-    p = max(eps_ii) / (2 * sigma ** 2)
+    p = 1 / (2 * sigma ** 2)
     
     gaussian_vals = np.exp(- p * ξ2)
     
+    flat_indices = flat_indices % n123 
     vr_sum = sum(mesh[flat_indices[:, 0], 
                       flat_indices[:, 1], 
                       flat_indices[:, 2]] * gaussian_vals)
@@ -113,8 +139,17 @@ def gaussian_integrate(AL, mesh: np.ndarray, window_lb, window_ur,
     vr_avg = vr_sum / gaussian_sum
     
     gaussian_sum *= 1/sqrt(prod(eps_ii)) * sqrt(p / pi)**3 * dΩ
-    if (debug := True) and gaussian_sum < 0.9:
-        print(f"{window_lb=}, {window_ur=}, guassian_sum={gaussian_sum}")
+    if (verbose := True) and not (0.9 < gaussian_sum < 1.0):
+        print(f"{gaussian_sum:.6f}->(guassian_sum) {n123 = } \n"
+              # f"\t{eps_ii=}, {inv_eps=}\n"
+              f"\t{window_lb=}, {window_ur=} \n"
+              f"\t{pos@AL=}")
+        if debug := False:
+            print(f"{pos=}")
+            print(f"{rT[:]}")
+            print(f"{flat_indices[:]}")
+            print(f"{flat_indices/n123[:]}")
+            print(f"{(flat_indices / n123 - pos)=}")
     
     return r, ξ, vr_avg
 
@@ -145,14 +180,17 @@ def write_diffvatom(filename: str, bulk: AtomConfig, supcl: AtomConfig,
         f.write(f" {supcl.natoms} atoms\n")
         f.write(" EPSILON\n")
         for i in range(3):
-            f.write(f"{epsilon[i, 0]:15.8f} {epsilon[i, 1]:15.8f} {epsilon[i, 2]:15.8f}\n")
+            f.write("%15.8f %15.8f %15.8f\n" % (
+                epsilon[i, 0], epsilon[i, 1], epsilon[i, 2]))
         f.write(" diff_VATOM  # r, ξ, 1/√(ε₁ε₂ε₃)/ξ, "
                 "bulk.ac.itypes, bulk.ac.positions[0:3], bulk.vatom, "
                 "supcl.ac.itypes, supcl.ac.positions[0:3], supcl.vatom, "
                 "diff_vatom\n")
         for i in range(supcl.natoms):
-            k = order[i]
+            k = order[i]                                                                             
             f.write(f"{r[i]:15.8f} {ξ[i]:15.8f} {pointv[i]:15.8f} ")
-            f.write(f"{bulk.itypes[k]:20d} {bulk.positions[k, 0]:15.8f} {bulk.positions[k, 1]:15.8f} {bulk.positions[k, 2]:15.8f} {diff_vatom[i, 0]:15.8f}")
-            f.write(f"{supcl.itypes[i]:20d} {supcl.positions[i, 0]:15.8f} {supcl.positions[i, 1]:15.8f} {supcl.positions[i, 2]:15.8f} {diff_vatom[i, 1]:15.8f}")
+            f.write("%20d %15.8f %15.8f %15.8f %15.8f" % (
+                bulk.itypes[k], *bulk.positions[k], diff_vatom[i, 0]))
+            f.write("%20d %15.8f %15.8f %15.8f %15.8f" % (
+                supcl.itypes[i], *supcl.positions[i], diff_vatom[i, 1]))
             f.write(f"{diff_vatom[i, 2]:20.8f}\n")
